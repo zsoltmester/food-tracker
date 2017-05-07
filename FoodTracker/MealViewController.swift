b50101ccb6da26c84e9bd99c6a9d4559a1c3745f
Mester, Zsolt <zsolt.mester@misys.com>
2017-05-07 23:29:13 +0200

    Doing the 6th lesson.

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MealViewController.swift
 //  FoodTracker
 //
 //  Created by Zsolt Mester on 2017. 04. 24..
@@ -8,12 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
 	//MARK: Properties
 
 	@IBOutlet weak var nameTextField: UITextField!
-	@IBOutlet weak var mealNameLabel: UILabel!
 	@IBOutlet weak var photoImageView: UIImageView!
 	@IBOutlet weak var ratingControl: RatingControl!
 
@@ -33,7 +32,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
 	}
 
 	func textFieldDidEndEditing(_ textField: UITextField) {
-		mealNameLabel.text = textField.text
 	}
 
 	//MARK: UIImagePickerControllerDelegate
